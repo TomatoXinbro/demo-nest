97510aafc92dbc3920e71370e876550cb1873657
potato <potato.coderx@gmail.com>
2024-03-25 17:11:17 +0800

    ✨ feat: ddd

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,13 +1,20 @@
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
+import Image from 'next/image';
 
 export default function AcmeLogo() {
   return (
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
+      <Image
+        src="/potato.jpg"
+        alt="error"
+        width={50}
+        height={50}
+        className="mr-[12px] rounded-[50%]"
+      />
+      <p className="text-[44px]">Potato</p>
     </div>
   );
 }
